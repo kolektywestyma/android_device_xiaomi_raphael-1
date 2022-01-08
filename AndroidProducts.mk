ce781753f48d2a909919a9bff8948a59508cce82
kolektywestyma <estymkakolektyw@gmail.com>
2022-01-08 23:43:16 +0100

    test2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_raphael.mk
+    $(LOCAL_DIR)/p404_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_raphael-user \
-    aosp_raphael-userdebug \
-    aosp_raphael-eng
+    p404_raphael-user \
+    p404_raphael-userdebug \
+    p404_raphael-eng
