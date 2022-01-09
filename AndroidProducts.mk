983523241e127f5168d477a9cefcdeeac786b26c
kolektywestyma <estymkakolektyw@gmail.com>
2022-01-09 17:28:32 +0100

    t3

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_raphael.mk
+    $(LOCAL_DIR)/lineage_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_raphael-userdebug \
-    arrow_raphael-eng
+    lineage_raphael-userdebug \
+    lineage_raphael-eng
