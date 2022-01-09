1c75282eb80f7ce3200dbb6720f737f0eaad9206
kolektywestyma <estymkakolektyw@gmail.com>
2022-01-09 12:07:05 +0100

    t1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_raphael.mk
+    $(LOCAL_DIR)/elixir_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_raphael-userdebug \
-    arrow_raphael-eng
+    elixir_raphael-userdebug \
+    elixir_raphael-eng
