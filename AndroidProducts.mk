1182f9c6bedd93cecea3a943f97ab2dec4130cb4
kolektywestyma <estymkakolektyw@gmail.com>
2022-01-18 22:33:07 +0100

    fix build file

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_raphael.mk
+    $(LOCAL_DIR)/potato_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_raphael-user \
-    voltage_raphael-userdebug \
-    voltage_raphael-eng
+    potato_raphael-user \
+    potato_raphael-userdebug \
+    potato_raphael-eng
